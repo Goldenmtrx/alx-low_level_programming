946128377f3a9153f2d5147df79d9f563de44b54
Ayoub <hassankoba1999@gmail.com>
2023-07-05 12:44:14 -0700

    commit

--- 2-strlen_recursion.c
+++ 2-strlen_recursion.c
@@ -8,12 +8,12 @@
 
 int _strlen_recursion(char *s)
 {
-	int ntr = 0;
+int a = 0;
 
-	if (*s > '\0')
-	{
-		ntr += _strlen_recursion(s + 1) + 1;
-	}
+if (*s > '\0')
+{
+	a += _strlen_recursion(s + 1) + 1;
+}
 
-	return (ntr);
+return (a);
 }
