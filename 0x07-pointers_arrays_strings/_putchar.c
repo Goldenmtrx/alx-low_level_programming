a9c6c9ce86799f445f2f3800433a7d44a7838dd7
Goldenmtrx <133365676+Goldenmtrx@users.noreply.github.com>
2023-07-05 03:39:38 +0100

    Commit

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -1,1 +1,14 @@
-wg
+#include <unistd.h>
+
+/**
+ * _putchar : - writes the char c to stdout
+ * @c: the char to print 
+ *
+ * Return: on success1.
+ * on error, -1 is returned, and ernno
+*/
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
