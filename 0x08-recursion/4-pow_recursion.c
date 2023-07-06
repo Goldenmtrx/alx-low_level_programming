f09e76650e246dcf7cec3b8af6ad702c241faa8b
Ayoub <hassankoba1999@gmail.com>
2023-07-05 18:02:41 -0700

    com

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -14,8 +14,13 @@ int _pow_recursion(int x, int y)
 	if (y < 0)
 		return (-1);
 	if else(y == 1)
+	{
 		return (x);
+	}
 	if else(y == 0)
+	{
 		return (1);
+	}
+
 	return (x * _pow_recursion(x, y - 1));
 }
