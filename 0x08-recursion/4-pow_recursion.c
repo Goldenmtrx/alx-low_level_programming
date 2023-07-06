469b2819790f19ffb74dbe97ca3b02e2a8b8d92c
Ayoub <hassankoba1999@gmail.com>
2023-07-05 18:00:25 -0700

    com

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,7 +4,7 @@
  * _pow_recursion - search a string for any of a set of bytes.
  * @x: base
  * @y: exposant
- * Return: pointer to the byte in 's' that matches one of the bytes in 'accept'
+ * Return: pointer to the byte in 's ' that matches one of the bytes in 'accept'
  * or null if no such byte is found
 */
 
